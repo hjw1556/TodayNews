2b5f0596145c7fc334e4b11de6ce161f1e50f25f
jiaweihuang <jiawei.huang@datacvg.com>
2020-04-05 10:32:24 +0800

    Initial Commit

---
+++ TodayNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodayNews
+//
+//  Created by 黄佳玮 on 2020/4/5.
+//  Copyright © 2020 黄佳玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TodayNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodayNews
+//
+//  Created by 黄佳玮 on 2020/4/5.
+//  Copyright © 2020 黄佳玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
